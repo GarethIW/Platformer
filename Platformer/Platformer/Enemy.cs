af321a7d90070415b1abf4cefa124c21eb8d68e0
Gareth Williams <gareth@garethw.net>
2013-11-05 16:24:12 +0000

    Enemy AI done

--- Platformer/Platformer/Enemy.cs
+++ Platformer/Platformer/Enemy.cs
@@ -29,7 +29,7 @@ namespace Platformer
         int currentFrame = 0;
         double currentFrameTime = 0;
 
-        int faceDir = Platformer.Rand.Next(2);
+        int faceDir = 1;
 
         // Collisions Bounds
         Rectangle collRect;
@@ -42,11 +42,68 @@ namespace Platformer
             Position = pos;
 
             spriteSheet = sheet;
+
+            faceDir = Platformer.Rand.Next(2);
+            if (faceDir == 0) faceDir = -1;
+
+            // Set up collision rectangle
+            collRect = new Rectangle(0, 0, FRAME_WIDTH - 20, FRAME_HEIGHT - 20);
         }
 
         public void Update(GameTime gameTime, Map gameMap)
         {
+            MoveHorizontal(faceDir);
 
+            // Move rectangle to enemy's location
+            collRect.X = (int)Position.X - (collRect.Width / 2);
+            collRect.Y = (int)Position.Y - collRect.Height;
+
+            // Apply inertia to X speed
+            Speed.X = MathHelper.Lerp(Speed.X, 0f, INERTIA);
+            // Clamp X floating point value
+            if ((Speed.X > 0f && Speed.X < 0.1f) || (Speed.X < 0f && Speed.X > -0.1f)) Speed.X = 0f;
+            // Apply gravity to Y speed
+            Speed.Y += Map.GRAVITY;
+
+            // Check Collisions
+            CheckCollisions(gameMap);
+
+            // Check platform edges
+            CheckPlatform(gameMap);
+
+            // Apply speed to position
+            Position += Speed;
+
+            // Clamp X position to map boundaries
+            if (Position.X < 0)
+            {
+                Position.X = 0;
+                Speed.X = 0;
+                faceDir = 1;
+            }
+            if (Position.X > Map.MAP_WIDTH * Map.TILE_WIDTH)
+            {
+                Position.X = Map.MAP_WIDTH * Map.TILE_WIDTH;
+                Speed.X = 0;
+                faceDir = -1;
+            }
+            
+            // If we're not jumping...
+            if (Speed.X != 0f)
+            {
+                // If we're moving left or right, animate!
+                // Increment frame time counter by elapsed game frame time (ms)
+                currentFrameTime += gameTime.ElapsedGameTime.TotalMilliseconds;
+                if (currentFrameTime >= FRAME_TIME)
+                {
+                    // If current animation frame time is reached, reset current frame time to 0 and increment current frame number
+                    currentFrameTime = 0;
+                    currentFrame++;
+                    // If we're at the end of the animation sequence, loop back to first frame
+                    if (currentFrame >= WALK_FRAMES + FIRST_WALK_FRAME) currentFrame = FIRST_WALK_FRAME;
+                }
+            }
+                
         }
 
         public void MoveHorizontal(int dir)
@@ -68,6 +125,85 @@ namespace Platformer
                     1f,
                     faceDir == 1 ? SpriteEffects.None : SpriteEffects.FlipHorizontally,
                     0f);
+        }
+
+        private void CheckCollisions(Map gameMap)
+        {
+            Vector2 checkPos;
+
+            // First, check collsions downwards
+            if (Speed.Y > 0)
+            {
+                checkPos.Y = collRect.Bottom + Speed.Y;
+                for (checkPos.X = collRect.Left + 4; checkPos.X <= collRect.Right - 4; checkPos.X += collRect.Width / 8)
+                    if (gameMap.GetTileAt(checkPos) > 0)
+                    {
+                        Speed.Y = 0;
+                    }
+            }
+
+            //  Up
+            if (Speed.Y < 0)
+            {
+                checkPos.Y = collRect.Top + Speed.Y;
+                for (checkPos.X = collRect.Left + 4; checkPos.X <= collRect.Right - 4; checkPos.X += collRect.Width / 8)
+                    if (gameMap.GetTileAt(checkPos) > 0)
+                    {
+                        Speed.Y = 0;
+                    }
+            }
+
+            // Left
+            if (Speed.X < 0)
+            {
+                checkPos.X = collRect.Left + Speed.X;
+                for (checkPos.Y = collRect.Top + 4; checkPos.Y <= collRect.Bottom - 4; checkPos.Y += collRect.Height / 8)
+                    if (gameMap.GetTileAt(checkPos) > 0)
+                    {
+                        Speed.X = 0;
+                        faceDir = 1;
+                    }
+            }
+
+            // Right
+            if (Speed.X > 0)
+            {
+                checkPos.X = collRect.Right + Speed.X;
+                for (checkPos.Y = collRect.Top + 4; checkPos.Y <= collRect.Bottom - 4; checkPos.Y += collRect.Height / 8)
+                    if (gameMap.GetTileAt(checkPos) > 0)
+                    {
+                        Speed.X = 0;
+                        faceDir = -1;
+                    }
+            }
+
+        }
+
+        private void CheckPlatform(Map gameMap)
+        {
+            Vector2 checkPos;
+
+            // Left
+            if (Speed.X < 0)
+            {
+                checkPos.X = collRect.Left - (Map.TILE_WIDTH/2);
+                checkPos.Y = collRect.Bottom + (Map.TILE_HEIGHT / 2);
+                if (gameMap.GetTileAt(checkPos) == 0)
+                {
+                    faceDir = 1;
+                }
+            }
+
+            // Right
+            if (Speed.X > 0)
+            {
+                checkPos.X = collRect.Right + (Map.TILE_WIDTH/2);
+                checkPos.Y = collRect.Bottom + (Map.TILE_HEIGHT / 2);
+                if (gameMap.GetTileAt(checkPos) == 0)
+                {
+                    faceDir = -1;
+                }
+            }
         }
     }
 }
